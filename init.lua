6f4acfd5021f9147f511a39ded3a997f5e963eed
Mirza Hilmi <dev@mrz.my.id>
2025-01-25 18:54:48 +0700

    feat(blink.cmp): use nvim_cmp highlight group

--- init.lua
+++ init.lua
@@ -192,6 +192,7 @@ local plugins = {
     after = function()
       require("blink.cmp").setup {
         appearance = {
+          use_nvim_cmp_as_default = true,
           nerd_font_variant = "normal",
         },
       }
