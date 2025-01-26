b613ac2da66761cc458664a08a3385a9fe15b5f7
Mirza Hilmi <dev@mrz.my.id>
2025-01-26 19:13:45 +0700

    feat(fzf-lua): set fzf-lua as vim.ui.select

--- init.lua
+++ init.lua
@@ -363,6 +363,16 @@ local plugins = {
           },
         },
       }
+      fzflua.register_ui_select(function(_, items)
+        local min_h, max_h = 0.15, 0.70
+        local h = (#items + 4) / vim.o.lines
+        if h < min_h then
+          h = min_h
+        elseif h > max_h then
+          h = max_h
+        end
+        return { winopts = { height = h, width = 0.60, row = 0.40 } }
+      end)
       vim.keymap.set("n", "<leader>sf", fzflua.files)
       vim.keymap.set("n", "<leader>sg", fzflua.live_grep)
       vim.keymap.set("n", "<leader>/", fzflua.lgrep_curbuf)
