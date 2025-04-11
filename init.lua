ffdd0d96502fb70479fc04dce73b703c901b0d42
Mirza Hilmi <dev@mrz.my.id>
2025-04-12 00:12:15 +0700

    refactor: show diagnostic text only upon current line

--- init.lua
+++ init.lua
@@ -48,16 +48,12 @@ vim.filetype.add { extension = { zul = "html" } }
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 -- Silent deprecation message
+---@diagnostic disable-next-line: duplicate-set-field
 vim.deprecate = function() end
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>", { silent = true })
--- Diagnostic keymaps
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 -- Exit terminal mode in the builtin terminal.
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 -- Disable arrow keys in normal mode
@@ -153,6 +149,8 @@ local plugins = {
               },
             }
           end)
+
+          vim.diagnostic.config { virtual_text = { current_line = true } }
 
           local client = vim.lsp.get_client_by_id(event.data.client_id)
           if client and client.server_capabilities.documentHighlightProvider then
