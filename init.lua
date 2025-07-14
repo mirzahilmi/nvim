d7278724c89bb3683d008e7fc12990480655ab92
mirza@t4nix <dev@mrz.my.id>
2025-07-14 12:37:35 +0700

    refactor(clipboard): disable wsl clipboard temporarily

--- init.lua
+++ init.lua
@@ -9,22 +9,22 @@ vim.api.nvim_create_autocmd({ "FocusLost" }, {
 })
 
 -- see https://www.reddit.com/r/neovim/comments/1byy8lu/copying_to_the_windows_clipboard_from_wsl2
-if vim.fn.has "wsl" then
-  vim.g.clipboard = {
-    name = "win_clipboard",
-    copy = {
-      ["+"] = "clip.exe",
-      ["*"] = "clip.exe",
-    },
-    paste = {
-      ["+"] = "powershell.exe Get-Clipboard",
-      ["*"] = "powershell.exe Get-Clipboard",
-    },
-    cache_enabled = 0,
-  }
-  vim.keymap.set({ "n", "v" }, "y", '"+y', { noremap = true, silent = true })
-  vim.keymap.set({ "n", "v" }, "p", '"+p', { noremap = true, silent = true })
-end
+-- if vim.fn.has "wsl" then
+--   vim.g.clipboard = {
+--     name = "win_clipboard",
+--     copy = {
+--       ["+"] = "clip.exe",
+--       ["*"] = "clip.exe",
+--     },
+--     paste = {
+--       ["+"] = "powershell.exe Get-Clipboard",
+--       ["*"] = "powershell.exe Get-Clipboard",
+--     },
+--     cache_enabled = 0,
+--   }
+--   vim.keymap.set({ "n", "v" }, "y", '"+y', { noremap = true, silent = true })
+--   vim.keymap.set({ "n", "v" }, "p", '"+p', { noremap = true, silent = true })
+-- end
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
