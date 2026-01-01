8548bb1fe37e1e2ee9f87b7ed0dde0cf3fde30eb
mirzaganteng <dev@mrz.my.id>
2026-01-01 12:09:11 +0700

    refactor: re-enable arrow navigation for mobile usage

--- init.lua
+++ init.lua
@@ -75,11 +75,6 @@ vim.keymap.set("n", "<Esc>", function()
 end, { silent = true })
 -- Exit terminal mode in the builtin terminal.
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
--- Disable arrow keys in normal mode
-vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 -- Keybinds to make split navigation easier.
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
